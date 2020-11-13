0ee783a9126a3cc16292c2d7f3311170b6c94769
AlexeyPot <potap2alex@gmail.com>
2020-11-13 14:17:01 +0200

    Resolves #1

---
+++ Tables/dbo.CV.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[CV] (
+  [CVID] [int] IDENTITY,
+  [Resume] [varbinary](max) NULL,
+  [FName] [varchar](50) NULL,
+  [FileExtension] [varchar](4) NULL,
+  CONSTRAINT [PK_CVID] PRIMARY KEY CLUSTERED ([CVID])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
