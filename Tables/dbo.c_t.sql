237bbbdb17b6714059a0566c868b2405a3a5eecf
AlexeyPot <potap2alex@gmail.com>
2020-11-13 13:52:32 +0200

    Closed #1

---
+++ Tables/dbo.c_t.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[c_t] (
+  [id] [int] NOT NULL,
+  [column2] [varchar](50) NULL,
+  CONSTRAINT [PK_c_t_id] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[c_t]
+  ADD CONSTRAINT [FK_c_t_id] FOREIGN KEY ([id]) REFERENCES [dbo].[p_t] ([id])
+GO
---
+++ Tables/dbo.p_t.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[p_t] (
+  [id] [int] IDENTITY,
+  [column2] [varchar](50) NULL,
+  CONSTRAINT [PK_p_t_id] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
