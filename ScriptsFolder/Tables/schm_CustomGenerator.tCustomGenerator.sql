28838a6162d15a4f7c6f0460db64853f5cb16eb2
AlexeyPot <potap2alex@gmail.com>
2020-11-18 18:32:54 +0200

    table was replaced

--- ScriptsFolder/Tables/schm_CustomGenerator.tCustomGenerator.sql
+++ ScriptsFolder/Tables/schm_CustomGenerator.tCustomGenerator.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [schm_CustomGenerator].[tCustomGenerator] (
   [id] [int] IDENTITY,
-  [col1] [varchar](50) NOT NULL,
-  [col2] [varchar](50) NOT NULL,
+  [col1] [varchar](50) NULL,
+  [col2] [varchar](50) NULL,
   CONSTRAINT [PK_CustomGeneratorId] PRIMARY KEY CLUSTERED ([id])
 )
 ON [PRIMARY]
--- ScriptsFolder/schm_CustomGenerator.tCustomGenerator.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [schm_CustomGenerator].[tCustomGenerator] (
-  [id] [int] IDENTITY,
-  [col1] [varchar](50) NULL,
-  [col2] [varchar](50) NULL,
-  CONSTRAINT [PK_CustomGeneratorId] PRIMARY KEY CLUSTERED ([id])
-)
-ON [PRIMARY]
-GO
