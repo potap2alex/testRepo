c258fc979d0f7e0006ef6ea76a073384261c5457
AlexeyPot <potap2alex@gmail.com>
2020-11-18 18:23:49 +0200

    NULL columns were changed

---
+++ ScriptsFolder/schm_CustomGenerator.tCustomGenerator.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [schm_CustomGenerator].[tCustomGenerator] (
+  [id] [int] IDENTITY,
+  [col1] [varchar](50) NULL,
+  [col2] [varchar](50) NULL,
+  CONSTRAINT [PK_CustomGeneratorId] PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
