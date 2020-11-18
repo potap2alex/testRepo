fec0c12766cb4c5c3e568106f4cb6dc08c26814c
AlexeyPot <potap2alex@gmail.com>
2020-11-18 18:42:31 +0200

    first version tables was returned

--- ScriptsFolder/Tables/schm_CustomGenerator.tCustomGenerator.sql
+++ ScriptsFolder/Tables/schm_CustomGenerator.tCustomGenerator.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [schm_CustomGenerator].[tCustomGenerator] (
-  [id] [int] IDENTITY,
-  [col1] [varchar](50) NULL,
-  [col2] [varchar](50) NULL,
+  [id] [int] NOT NULL,
+  [col1] [varchar](50) NOT NULL,
+  [col2] [varchar](50) NOT NULL,
   CONSTRAINT [PK_CustomGeneratorId] PRIMARY KEY CLUSTERED ([id])
 )
 ON [PRIMARY]
