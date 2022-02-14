883537c416d34ce3209bf75ea50543e49eaca5fb
AlexeyPot <AlexeyPot@devart.com>
2022-02-14 08:52:05 +0200

    Database with Static Data was added

---
+++ test_StaticData/Tables/StaticData/dbo.city.Data.sql
@@ -0,0 +1,7 @@
+﻿SET IDENTITY_INSERT dbo.city ON
+GO
+INSERT dbo.city(city_id, city, country_id) VALUES (1, 'Toronto', 2);
+INSERT dbo.city(city_id, city, country_id) VALUES (2, 'Boston', 1);
+GO
+SET IDENTITY_INSERT dbo.city OFF
+GO
