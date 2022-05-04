9a516079308ec92d49f5607bcec8f59b46b656ce
AlexeyPot <AlexeyPot@devart.com>
2022-05-04 16:50:31 +0300

    commit table with trigger

---
+++ TableThatAddedATrigger/Tables/dbo.t_rebuild.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[t_rebuild] (
+  [id1] [int] NULL,
+  [id2] [int] NULL
+)
+ON [PRIMARY]
+GO
+
+SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE TRIGGER [dbo].[trg_dml_on_table]
+  ON [t_rebuild]
+  FOR UPDATE
+AS
+SET NOCOUNT ON;
+GO
