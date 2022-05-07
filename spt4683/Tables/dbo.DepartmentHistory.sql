0475ee55008b60a52641e99c673b2b492777d2d6
AlexeyPot <AlexeyPot@devart.com>
2022-05-07 15:41:07 +0300

    history was added

---
+++ spt4683/Tables/dbo.DepartmentHistory.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[DepartmentHistory] (
+  [DeptID] [int] NOT NULL,
+  [DeptName] [varchar](50) NOT NULL,
+  [ManagerID] [int] NULL,
+  [ParentDeptID] [int] NULL,
+  [ValidFrom] [datetime2] NOT NULL,
+  [ValidTo] [datetime2] NOT NULL
+)
+ON [PRIMARY]
+GO
+
+CREATE CLUSTERED COLUMNSTORE INDEX [IX_DepartmentHistory]
+  ON [dbo].[DepartmentHistory]
+GO
+
+CREATE INDEX [IX_DepartmentHistory_ID_PERIOD_COLUMNS]
+  ON [dbo].[DepartmentHistory] ([ValidTo], [ValidFrom], [DeptID])
+  ON [PRIMARY]
+GO
