216ca23bc0aa446d0b2a4d3e504ae577df96fac9
AlexeyPot <AlexeyPot@devart.com>
2022-05-07 15:49:21 +0300

    department was added

---
+++ spt4683/Tables/dbo.Department.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Department] (
+  [DeptID] [int] NOT NULL,
+  [DeptName] [varchar](50) NOT NULL,
+  [ManagerID] [int] NULL,
+  [ParentDeptID] [int] NULL,
+  [ValidFrom] [datetime2] GENERATED ALWAYS AS ROW START NOT NULL,
+  [ValidTo] [datetime2] GENERATED ALWAYS AS ROW END NOT NULL,
+  PERIOD FOR SYSTEM_TIME (ValidFrom, ValidTo),
+  PRIMARY KEY CLUSTERED ([DeptID])
+)
+ON [PRIMARY]
+WITH (SYSTEM_VERSIONING = ON (HISTORY_TABLE = [dbo].[DepartmentHistory], DATA_CONSISTENCY_CHECK = ON))
+GO
